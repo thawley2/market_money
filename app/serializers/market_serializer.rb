18ec35bdbb9468ecb68237f03af448d97ca8d568
Thomas Hawley <thawley22@turing.edu>
2023-05-15 16:50:03 -0600

    refactor: Add vendor_count to attributes

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,8 +1,13 @@
 class MarketSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :street, :city, :county, :state, :zip, :lat, :lon
-
-  attribute :vendor_count do |market|
-    market.count_of_vendors
-  end
+  attributes :id, 
+             :name, 
+             :street, 
+             :city, 
+             :county, 
+             :state, 
+             :zip, 
+             :lat, 
+             :lon, 
+             :vendor_count
 end
