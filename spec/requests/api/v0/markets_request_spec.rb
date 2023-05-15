bf08b3b4bb3709444639b0d5bc4fae1163bd0454
Thomas Hawley <thawley22@turing.edu>
2023-05-15 16:31:22 -0600

    test: Add complete test for /api/v0/markets

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -10,9 +10,33 @@ RSpec.describe 'Markets API' do
       get '/api/v0/markets'
 
       json = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
-      expect(json).to be_an Array
-      expect(json.count).to eq(5)
+      
+      expect(json[:data]).to be_an Array
+      expect(json[:data].count).to eq(5)
+      json[:data].each do |market|
+        expect(market).to have_key(:id)
+        expect(market).to have_key(:type)
+        expect(market[:attributes]).to be_a Hash
+        expect(market[:attributes]).to have_key(:name)
+        expect(market[:attributes]).to have_key(:street)
+        expect(market[:attributes]).to have_key(:city)
+        expect(market[:attributes]).to have_key(:county)
+        expect(market[:attributes]).to have_key(:state)
+        expect(market[:attributes]).to have_key(:zip)
+        expect(market[:attributes]).to have_key(:lat)
+        expect(market[:attributes]).to have_key(:lon)
+        expect(market[:attributes]).to have_key(:vendor_count)
+
+        expect(market[:attributes][:name]).to be_a String
+        expect(market[:attributes][:street]).to be_a String
+        expect(market[:attributes][:city]).to be_a String
+        expect(market[:attributes][:county]).to be_a String
+        expect(market[:attributes][:state]).to be_a String
+        expect(market[:attributes][:zip]).to be_a String
+        expect(market[:attributes][:lat]).to be_a String
+        expect(market[:attributes][:lon]).to be_a String
+        expect(market[:attributes][:vendor_count]).to be_an Integer
+      end
     end
   end
 end
