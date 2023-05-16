aa8efd77d3815ae3d1bb5b0cb9d1ca186c6d76be
Thomas Hawley <thawley22@turing.edu>
2023-05-15 20:32:26 -0600

    feat: Add show action along with rescue_from and error message

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,5 +1,23 @@
 class Api::V0::MarketsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :no_record
+  
   def index
     render json: MarketSerializer.new(Market.all)
   end
+
+  def show
+    render json: MarketSerializer.new(Market.find(params[:id]))
+  end
+
+  private
+    def no_record
+      error_message = { 
+        errors: [
+          {
+            detail: "Couldn't find Market with 'id'=#{params[:id]}"
+            }
+            ]
+          }
+      render json: error_message
+    end
 end
