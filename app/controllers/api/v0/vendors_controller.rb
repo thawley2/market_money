e618f296bfb62d20705d5fd9a8e88e55b5fceb13
Thomas Hawley <thawley22@turing.edu>
2023-05-16 15:36:44 -0600

    feat: Add create action to vendors controller

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -4,22 +4,23 @@ class Api::V0::VendorsController < ApplicationController
     begin
       market = Market.find(params[:market_id])
       render json: VendorSerializer.new(market.vendors)
-    rescue ActiveRecord::RecordNotFound => exception
-      error_handling(exception)
     end
   end
 
   def show
-    begin
       vendor = Vendor.find(params[:id])
       render json: VendorSerializer.new(Vendor.find(params[:id]))
-    rescue ActiveRecord::RecordNotFound => exception
-      error_handling(exception)
+  end
+
+  def create
+    vendor = Vendor.new(vendor_params)
+    if vendor.save
+      render json: VendorSerializer.new(Vendor.last), status: 201
     end
   end
 
   private
-    def error_handling(error)
-      render json: ErrorSerializer.serialize(error), status: 404
+    def vendor_params
+      params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
     end
 end
