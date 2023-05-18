34cb6f3f9070b1219c206d0cc93c00d5e5fcddb5
Thomas Hawley <thawley22@turing.edu>
2023-05-17 22:29:37 -0600

    feat: Add route destroy to marketvendors

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :vendors, only: [:show, :create, :update, :destroy]
       resources :market_vendors, only: [:create]
+      resource :market_vendors, only: [:destroy]
       resources :markets, only: [:index, :show] do
         resources :vendors, only: [:index], controller: 'markets/vendors'
       end
