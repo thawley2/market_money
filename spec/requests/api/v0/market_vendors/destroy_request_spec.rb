759fbb0134ba8899f69df7a4ff7f838fc1bf23db
Thomas Hawley <thawley22@turing.edu>
2023-05-17 22:52:00 -0600

    test: Add happy and sad path testing for deleting a marketvendor record

--- spec/requests/api/v0/market_vendors/destroy_request_spec.rb
+++ spec/requests/api/v0/market_vendors/destroy_request_spec.rb
@@ -29,5 +29,71 @@ RSpec.describe 'MarketVendor API' do
 
         expect(vendors[:data]).to eq([])
     end
+
+    it 'Sends an error message when the marketvendor record can not be found' do
+      mv_params = {
+        "market_id": "#{@market4.id}",
+        "vendor_id": "#{@vendor1.id}"
+        }
+      headers = {"CONTENT_TYPE" => "application/json"}
+      delete "/api/v0/market_vendors", headers: headers, params: JSON.generate(mv: mv_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      error_deets = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_deets).to be_a Hash
+      expect(error_deets).to have_key(:errors)
+      expect(error_deets[:errors]).to be_an Array
+      expect(error_deets[:errors][0]).to be_a Hash
+      expect(error_deets[:errors][0]).to have_key(:detail)
+      expect(error_deets[:errors][0][:detail]).to be_a String
+      expect(error_deets[:errors][0][:detail]).to eq("No MarketVendor with market_id=#{@market4.id} AND vendor_id=#{@vendor1.id} exists")
+    end
+
+    it 'Sends the same error if the market id doesnt exist' do
+      mv_params = {
+        "market_id": "1000000",
+        "vendor_id": "#{@vendor1.id}"
+        }
+      headers = {"CONTENT_TYPE" => "application/json"}
+      delete "/api/v0/market_vendors", headers: headers, params: JSON.generate(mv: mv_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      error_deets = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_deets).to be_a Hash
+      expect(error_deets).to have_key(:errors)
+      expect(error_deets[:errors]).to be_an Array
+      expect(error_deets[:errors][0]).to be_a Hash
+      expect(error_deets[:errors][0]).to have_key(:detail)
+      expect(error_deets[:errors][0][:detail]).to be_a String
+      expect(error_deets[:errors][0][:detail]).to eq("No MarketVendor with market_id=1000000 AND vendor_id=#{@vendor1.id} exists")
+    end
+
+    it 'Sends the same error if the market id is missing' do
+      mv_params = {
+        "market_id": "",
+        "vendor_id": "#{@vendor1.id}"
+        }
+      headers = {"CONTENT_TYPE" => "application/json"}
+      delete "/api/v0/market_vendors", headers: headers, params: JSON.generate(mv: mv_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      error_deets = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error_deets).to be_a Hash
+      expect(error_deets).to have_key(:errors)
+      expect(error_deets[:errors]).to be_an Array
+      expect(error_deets[:errors][0]).to be_a Hash
+      expect(error_deets[:errors][0]).to have_key(:detail)
+      expect(error_deets[:errors][0][:detail]).to be_a String
+      expect(error_deets[:errors][0][:detail]).to eq("No MarketVendor with market_id= AND vendor_id=#{@vendor1.id} exists")
+    end
   end
 end
