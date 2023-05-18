ba658f31e1a8b70c31b391fbf4f2e17c2517753e
Thomas Hawley <thawley22@turing.edu>
2023-05-18 17:10:40 -0600

    refactor: Add search action to markets controller

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -7,4 +7,25 @@ class Api::V0::MarketsController < ApplicationController
   def show
       render json: MarketSerializer.new(Market.find(params[:id]))
   end
+
+  def search
+    if params.key?(:state) && params.key?(:city) && params.key?(:name)
+      serialize(Market.search_state_city_name(params))
+    elsif params.key?(:state) && params.key?(:city)
+      serialize(Market.search_state_city(params))
+    elsif params.key?(:state) && params.key?(:name)
+      serialize(Market.search_state_name(params))
+    elsif params.key?(:state)
+      serialize(Market.search_state(params))
+    elsif params.key?(:name) && !params.key?(:city)
+      serialize(Market.search_name(params))
+    else
+      render json: ErrorSerializer.missing_search_parameters, status: 422
+    end
+  end
+
+  private
+    def serialize(objects)
+      render json: MarketSerializer.new(objects)
+    end
 end
