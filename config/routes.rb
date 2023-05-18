c26c97709ddc88229ab829d353c3da5d7df68de1
Thomas Hawley <thawley22@turing.edu>
2023-05-18 14:37:23 -0600

    feat: Add route search with show action only

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
       resources :vendors, only: [:show, :create, :update, :destroy]
       resources :market_vendors, only: [:create]
       resource :market_vendors, only: [:destroy]
+      namespace :markets do
+        resource :search, only: [:show]
+      end
       resources :markets, only: [:index, :show] do
         resources :vendors, only: [:index], controller: 'markets/vendors'
       end
