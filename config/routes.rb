0afaea4677cd5fe25ab0498caa21cc584f0a9527
Thomas Hawley <thawley22@turing.edu>
2023-05-17 20:24:45 -0600

    feat: Add route to market_vendors#create

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :vendors, only: [:show, :create, :update, :destroy]
+      resources :market_vendors, only: [:create]
       resources :markets, only: [:index, :show] do
         resources :vendors, only: [:index], controller: 'markets/vendors'
       end
