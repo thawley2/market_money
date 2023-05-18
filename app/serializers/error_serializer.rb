82d3ba64be723dbf683fedc314ff213f350586df
Thomas Hawley <thawley22@turing.edu>
2023-05-18 15:28:06 -0600

    feat: Add ::missing_search_parameters method

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -13,4 +13,9 @@ class ErrorSerializer
     message = "Validation failed: Market vendor association between market with market_id=#{mk_id} and vendor_id=#{vn_id} already exists"
     serialize(message)
   end
+
+  def self.missing_search_parameters
+    message = "Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint."
+    serialize(message)
+  end
 end
