239a1783d5234af137b0628125c0db262d409663
Thomas Hawley <thawley22@turing.edu>
2023-05-17 20:26:37 -0600

    refactor: Update error handling with better named methods, add logic to record_invalid to account for vendor_markets record already exists error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,28 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :error_handling
-  rescue_from ActiveRecord::RecordInvalid, with: :new_error_handling
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   private
-    def error_handling(error)
+  def record_invalid(error)
+    if error.to_s.include?('exist')
+      not_found(error)
+    elsif params.has_key?(:mv)
+      not_unique
+    else
+      field_blank(error)
+    end
+  end
+
+    def not_found(error)
       render json: ErrorSerializer.serialize(error), status: :not_found
     end
 
-    def new_error_handling(error)
+    def not_unique
+      render json: ErrorSerializer.serialize_uniq(params[:mv][:market_id], params[:mv][:vendor_id]), status: 422
+    end
+
+    def field_blank(error)
       render json: ErrorSerializer.serialize(error), status: :bad_request
     end
+
 end
