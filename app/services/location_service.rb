dc01d723ca429935250db851056ad9ab8d5b2531
Thomas Hawley <thawley22@turing.edu>
2023-05-18 22:19:03 -0600

    refactor: Add conn and get_url to private

--- app/services/location_service.rb
+++ app/services/location_service.rb
@@ -3,14 +3,15 @@ class LocationService
     get_url("/search/2/categorySearch/cash_dispenser.json?lat=#{lat}&lon=#{lon}")
   end
   
-  def conn
-    Faraday.new(url: 'https://api.tomtom.com') do |f|
-      f.params['key'] = ENV['TomTom_API_KEY']
+  private
+    def conn
+      Faraday.new(url: 'https://api.tomtom.com') do |f|
+        f.params['key'] = ENV['TomTom_API_KEY']
+      end
     end
-  end
-  
-  def get_url(url)
-    response = conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)
-  end
+    
+    def get_url(url)
+      response = conn.get(url)
+      JSON.parse(response.body, symbolize_names: true)
+    end
 end
