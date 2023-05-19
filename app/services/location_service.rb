f4e6818a34e1df8af8313d27dcbfd40f5b8809c6
Thomas Hawley <thawley22@turing.edu>
2023-05-18 19:49:47 -0600

    feat: Create location service with cash dispenser endpoint

---
+++ app/services/location_service.rb
@@ -0,0 +1,16 @@
+class LocationService
+  def nearby_atms(lat, lon)
+    get_url("/search/2/categorySearch/cash_dispenser.json?lat=#{lat}&lon=#{lon}")
+  end
+  
+  def conn
+    Faraday.new(url: 'https://api.tomtom.com') do |f|
+      f.params['key'] = ENV['TomTom_API_KEY']
+    end
+  end
+  
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
