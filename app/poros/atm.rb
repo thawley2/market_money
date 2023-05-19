c021d56b4cd7623004fb01560381adc1c88c8b39
Thomas Hawley <thawley22@turing.edu>
2023-05-18 22:18:00 -0600

    feat: Add id to attributes for serializer

--- app/poros/atm.rb
+++ app/poros/atm.rb
@@ -1,6 +1,7 @@
 class Atm
-  attr_reader :name, :address, :lat, :lon, :distance
+  attr_reader :name, :address, :lat, :lon, :distance, :id
   def initialize(atm_data)
+    @id = nil
     @name = atm_data[:name]
     @address = atm_data[:address]
     @lat = atm_data[:lat]
